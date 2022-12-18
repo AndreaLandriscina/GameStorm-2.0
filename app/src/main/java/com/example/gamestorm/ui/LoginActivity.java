c8602d9fa3488ce77101e414eaa322e033c5d8da
riccardo-borello-01 <r.borello@campus.unimib.it>
2022-12-18 12:41:58 +0100

    Login e registrazione sistemati

--- app/src/main/java/com/example/gamestorm/ui/LoginActivity.java
+++ app/src/main/java/com/example/gamestorm/ui/LoginActivity.java
@@ -1,5 +1,8 @@
 package com.example.gamestorm.ui;
 
+import static com.example.gamestorm.R.string.check_data;
+import static com.example.gamestorm.R.string.emailsent;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,9 +12,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.gamestorm.R;
 import com.example.gamestorm.databinding.ActivityLoginBinding;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -34,24 +39,30 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String email = binding.emailAddress.getText().toString().trim();
                 String password = binding.password.getText().toString().trim();
-                progressDialog.setTitle("Login...");
+                progressDialog.setTitle(getString(R.string.login_in_progress));
                 progressDialog.show();
-                firebaseAuth.signInWithEmailAndPassword(email,password)
-                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-                            @Override
-                            public void onSuccess(AuthResult authResult) {
-                                progressDialog.cancel();
-                                Toast.makeText(LoginActivity.this, "Login Successfully", Toast.LENGTH_SHORT).show();
-                                startActivity(new Intent(LoginActivity.this, MainActivity.class));
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                progressDialog.cancel();
-                                Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
-                            }
-                        });
+                if (isEmailOk(email) && isPasswordOk(password)) {
+                    firebaseAuth.signInWithEmailAndPassword(email, password)
+                            .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                @Override
+                                public void onSuccess(AuthResult authResult) {
+                                    progressDialog.cancel();
+                                    Toast.makeText(LoginActivity.this, R.string.login_successfully, Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    progressDialog.cancel();
+                                    Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                }else{
+                    Snackbar.make(LoginActivity.this.findViewById(android.R.id.content),
+                            check_data, Snackbar.LENGTH_SHORT).show();
+                    progressDialog.cancel();
+                }
             }
         });
 
@@ -59,31 +70,58 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String email=binding.emailAddress.getText().toString();
-                progressDialog.setTitle("Sending Mail...");
-                progressDialog.show();
-                firebaseAuth.sendPasswordResetEmail(email)
-                        .addOnSuccessListener(new OnSuccessListener<Void>() {
-                            @Override
-                            public void onSuccess(Void unused) {
-                                progressDialog.cancel();
-                                Toast.makeText(LoginActivity.this, "Email sent", Toast.LENGTH_SHORT).show();
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                progressDialog.cancel();
-                                Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
-                            }
-                        });
+                if(isEmailOk(email)) {
+                    progressDialog.setTitle(getString(R.string.sending_mail));
+                    progressDialog.show();
+                    firebaseAuth.sendPasswordResetEmail(email)
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    progressDialog.cancel();
+                                    Toast.makeText(LoginActivity.this, emailsent, Toast.LENGTH_SHORT).show();
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    progressDialog.cancel();
+                                    Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                }else{
+                    Snackbar.make(LoginActivity.this.findViewById(android.R.id.content),
+                            R.string.insert_mail_to_reset_password, Snackbar.LENGTH_SHORT).show();
+                    progressDialog.cancel();
+                }
             }
         });
 
         binding.goToSignUpActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+                finish();
             }
         });
+    }
+
+    private boolean isEmailOk(String email) {
+        if (email.isEmpty()) {
+            binding.emailAddress.setError(getString(R.string.email_empty));
+            return false;
+        } else {
+            binding.emailAddress.setError(null);
+            return true;
+        }
+    }
+
+    private boolean isPasswordOk(String password) {
+        if (password.isEmpty()) {
+            binding.password.setError(getString(R.string.password_empty));
+            return false;
+        } else {
+            binding.password.setError(null);
+            return true;
+        }
     }
 }
