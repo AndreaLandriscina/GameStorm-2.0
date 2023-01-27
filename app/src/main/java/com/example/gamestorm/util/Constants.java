c33e4d996addc6dfc27edf9216c0b97f614db60a
riccardo-borello-01 <r.borello@campus.unimib.it>
2023-01-27 17:33:38 +0100

    Conclusa parte utente

--- app/src/main/java/com/example/gamestorm/util/Constants.java
+++ app/src/main/java/com/example/gamestorm/util/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
     public static final String TOP_HEADLINES_ENDPOINT = "games";
     public static final String CLIENT_ID = "Client-ID";
     public static final String CLIENT_ID_VALUE = "w70u2nsn2gzl9g0xsbe3uvguva3zos";
-    public static final String TOKEN_API_VALUE = "Bearer sevk2qpth8qd683kwwl5e67m66t5ws";
+    public static final String TOKEN_API_VALUE = "Bearer 1w5btb4ipqlpouhec20h3vh4qtpzp7";
     public static final String TOKEN_API = "Authorization";
     public static final String CONTENT_TYPE = "Content-Type";
     public static final String CONTENT_TYPE_VALUE = "application/json;charset=utf-8";
