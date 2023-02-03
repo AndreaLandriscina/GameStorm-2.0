552f199cf2aac6b5196d097c7df5dde8e17b2316
Yassine Amine <y.amine@campus.unimib.it>
2023-02-03 18:39:28 +0100

    aggiunta la stringa in italiano per l'errore di connessione e corretti i riferimenti.

--- app/src/main/java/com/example/gamestorm/ui/DesiredFragment.java
+++ app/src/main/java/com/example/gamestorm/ui/DesiredFragment.java
@@ -96,7 +96,7 @@ public class DesiredFragment extends Fragment {
                     //viewGames();
                 Log.i("LOGGER","oncreateview");
             }  else{
-                Snackbar.make(view.findViewById(R.id.Coordinatorlyt), "No internet connection, please connect and retry.", Snackbar.LENGTH_LONG).show();
+                Snackbar.make(view.findViewById(R.id.Coordinatorlyt), R.string.no_connection_message, Snackbar.LENGTH_LONG).show();
             }
 
         }
@@ -125,7 +125,7 @@ public class DesiredFragment extends Fragment {
                 viewGames();
             }
         }else{
-            Snackbar.make(requireView().findViewById(R.id.Coordinatorlyt), "No internet connection, please connect and retry.", Snackbar.LENGTH_LONG).show();
+            Snackbar.make(requireView().findViewById(R.id.Coordinatorlyt), R.string.no_connection_message, Snackbar.LENGTH_LONG).show();
         }
     }
 
